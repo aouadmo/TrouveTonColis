0e9a2a78371d710e0367025da1ea0c205e9b5436
Clotilde <sall.clotilde@gmail.com>
2025-05-27 18:38:17 +0200

    debut back

---
+++ backend/models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.DB_URI;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
