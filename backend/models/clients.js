3ec5ae562d1111194ac008425c79fee5eb435dfe
SebastienH <sebhuguet06@gmail.com>
2025-05-27 18:02:47 +0200

    Formattage models/clients & pros

--- backend/models/clients.js
+++ backend/models/clients.js
@@ -1,24 +1,14 @@
-
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-
-
-
-// client
-
-nom : String,
-prenom : String,
-email : String,
-password : String,
-phone : Number,
-client : Boolean,
-token : String,
-
+    nom: String,
+    prenom: String,
+    email: String,
+    password: String,
+    phone: Number,
+    client: Boolean,
+    token: String,
 });
-
-
-
 
 const User = mongoose.model('users', userSchema);
 
