98a1b413385965172319050de2b7e97be214caca
Clotilde <sall.clotilde@gmail.com>
2025-05-28 20:35:10 +0200

    routes signup sigin fonctionnent

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,23 @@
 require('dotenv').config();
-
+require('./models/connection');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
-var clientsRouter = require('./routes/clients');
 var prosRouter = require('./routes/pros');
-
+// var colisRouter = require('./routes/colis');
+var usersRouter = require('./routes/users');
 var app = express();
-
 const cors = require('cors');
 app.use(cors());
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pros', prosRouter);
-
+// app.use('/colis', colisRouter);
 module.exports = app;
--- backend/routes/pros.js
+++ backend/routes/pros.js
@@ -1,29 +1,35 @@
 var express = require('express');
-var router = express.Router();
+const router = express.Router();
 
-require('../models/connection');
-const Pro = require('../models/pros');
+const Pro = require('../models/pro');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
 // Inscription Pro
-router.post('/pros/signup', (req, res) => {
-  if (!checkBody(req.body, ['client', 'phone2', 'adresse', 'ville', 'codePostal'])) {
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['nom', 'prenom', 'email', 'password', 'phone','nomRelais', 'adresse', 'ville', 'codePostal'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
   // Check si déjà existant ?
-  Pro.findOne({ username: req.body.email }).populate(client).then(data => {
+  Pro.findOne({ email: req.body.email }).then(data => {
     if (data === null) {
-      const hash = bcrypt.hashSync(req.body.password, 10);
 
-      const newPro = new Pro({ // pour la création de mon nouveau pro est-ce que je dois remettre toutes les clés du schéma client en plus de celles du schéma pro ? 
-        client: req.body.client,
-        phone2: req.body.phone2,
-        adresse: req.body.adress,
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const token = uid2(32);
+
+      const newPro = new Pro({  
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        email: req.body.email,
+        password: hash,
+        phone: req.body.phone,
+        nomRelais: req.body.nomRelais,
+        adresse: req.body.adresse,
         ville: req.body.ville,
         codePostal: req.body.codePostal,
+        token: token,
       });
 
       newPro.save().then(newDoc => {
@@ -37,13 +43,13 @@ router.post('/pros/signup', (req, res) => {
 
 
 // Connexion Pro
-router.post('/pros/signin', (req, res) => {
+router.post('/signin', (req, res) => {
   if (!checkBody(req.body, ['email', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
 
-Pro.findOne({ username: req.body.email }).then(data => {
+Pro.findOne({ email: req.body.email }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
@@ -54,3 +60,4 @@ Pro.findOne({ username: req.body.email }).then(data => {
 
 
 module.exports = router;
+
