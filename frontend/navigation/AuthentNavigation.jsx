3998a2bc5ea9d54a8a901552292acf206f59ba47
Clotilde <sall.clotilde@gmail.com>
2025-06-06 20:41:41 +0200

    fichiers sign up pro & client fusionné + navu-igation ok

--- frontend/navigation/AuthentNavigation.jsx
+++ frontend/navigation/AuthentNavigation.jsx
@@ -4,18 +4,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // Les Écrans
 import HomeScreen from '../screens/HomeScreen';
 import SignUpScreen from '../screens/SignUpScreen';
-import SignUpProScreen from '../screens/SignUpProScreen';
 import SearchScreen from '../screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
 
-export default function AuthNavigator() {
+export default function AuthentNavigator() {
     // Écrans disponibles avant la connexion (publics)
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-      <Stack.Screen name="SignUpProScreen" component={SignUpProScreen} />
       <Stack.Screen name ="SearchScreen" component={SearchScreen}/>
     </Stack.Navigator>
   );
